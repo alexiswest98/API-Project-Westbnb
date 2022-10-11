e715c574ce666ce2e7e8ca26a12c1f9b0f026b65
Alexis West <alexishwest98@gmail.com>
2022-10-10 19:35:07 -0700

    Add an API router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,13 +2,9 @@
 const express = require('express');
 const router = express.Router();
 
-// router.get('/hello/world', function(req, res) {
-//   res.cookie('XSRF-TOKEN', req.csrfToken());
-//   res.send('Hello World!');
-// });
+const apiRouter = require('./api');
+router.use('/api', apiRouter);
 
-// backend/routes/index.js
-// ...
 // Add a XSRF-TOKEN cookie
 router.get("/api/csrf/restore", (req, res) => {
   const csrfToken = req.csrfToken();
@@ -17,7 +13,7 @@ router.get("/api/csrf/restore", (req, res) => {
     'XSRF-Token': csrfToken
   });
 });
-// ...
+
 
 
 module.exports = router;
