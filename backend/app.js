05d2d4009cf4e21608e64673faaed79676770d6b
Alexis West <alexishwest98@gmail.com>
2022-10-11 21:54:38 -0700

    Add error-handling middleware for common errors and debugging

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+// backend/app.js
 const express = require('express');
 require('express-async-errors');
 const morgan = require('morgan');
@@ -5,17 +6,13 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
-// backend/app.js
 const routes = require('./routes');
-// ...
-
 const { environment } = require('./config');
+const app = express();
 const isProduction = environment === 'production';
 
-const app = express();
 
 app.use(morgan('dev'));
-
 app.use(cookieParser());
 app.use(express.json());
 
@@ -30,21 +27,57 @@ if (!isProduction) {
     helmet.crossOriginResourcePolicy({ 
       policy: "cross-origin" 
     })
-  );
-  
-  // Set the _csrf token and create req.csrfToken method
-  app.use(
-    csurf({
-      cookie: {
-        secure: isProduction,
-        sameSite: isProduction && "Lax",
-        httpOnly: true
-      }
-    })
-  );
+    );
+    
+    // Set the _csrf token and create req.csrfToken method
+    app.use(
+      csurf({
+        cookie: {
+          secure: isProduction,
+          sameSite: isProduction && "Lax",
+          httpOnly: true
+        }
+      })
+      );
+      
+      
+      
+      //Error Handling Middleware
+      app.use((_req, _res, next) => {
+        const err = new Error("The requested resource couldn't be found.");
+        err.title = "Resource Not Found";
+        err.errors = ["The requested resource couldn't be found."];
+        err.status = 404;
+        next(err);
+      });
+
+      
+      // Process sequelize errors
+      const { ValidationError } = require('sequelize');
+      
+      app.use((err, _req, _res, next) => {
+        // check if error is a Sequelize error:
+        if (err instanceof ValidationError) {
+          err.errors = err.errors.map((e) => e.message);
+          err.title = 'Validation error';
+        }
+        next(err);
+      });
+      
+      //Error formatter
+      app.use((err, _req, res, _next) => {
+        res.status(err.status || 500);
+        console.error(err);
+        res.json({
+          title: err.title || 'Server Error',
+          message: err.message,
+          errors: err.errors,
+          stack: isProduction ? null : err.stack
+        })
+      })
+      
+
 
 app.use(routes); // Connect all the routes
 
-// backend/app.js
-// ...
 module.exports = app;
