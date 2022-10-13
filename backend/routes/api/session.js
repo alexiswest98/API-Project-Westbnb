c6026e40ab6e4fe894478de3a80884dcfd6d8227
Alexis West <alexishwest98@gmail.com>
2022-10-13 13:36:29 -0700

    Add a backend endpoint to get the current user session

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -10,33 +10,46 @@ const router = express.Router();
 router.post(
     '/',
     async (req, res, next) => {
-      const { credential, password } = req.body;
-  
-      const user = await User.login({ credential, password });
-  
-      if (!user) {
-        const err = new Error('Login failed');
-        err.status = 401;
-        err.title = 'Login failed';
-        err.errors = ['The provided credentials were invalid.'];
-        return next(err);
-      }
-  
-      await setTokenCookie(res, user);
-  
-      return res.json({
-        user
-      });
-    }
-  );
+        const { credential, password } = req.body;
 
-  // Log out
+        const user = await User.login({ credential, password });
+
+        if (!user) {
+            const err = new Error('Login failed');
+            err.status = 401;
+            err.title = 'Login failed';
+            err.errors = ['The provided credentials were invalid.'];
+            return next(err);
+        }
+
+        await setTokenCookie(res, user);
+
+        return res.json({
+            user
+        });
+    }
+);
+
+// Log out
 router.delete(
     '/',
     (_req, res) => {
-      res.clearCookie('token');
-      return res.json({ message: 'success' });
+        res.clearCookie('token');
+        return res.json({ message: 'success' });
     }
-  );
+);
+
+router.get(
+    '/',
+    restoreUser,
+    (req, res) => {
+        const { user } = req;
+        if (user) {
+            return res.json({
+                user: user.toSafeObject()
+            });
+        } else return res.json({});
+    }
+);
 
 module.exports = router;
