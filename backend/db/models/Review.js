8c2277aa8f4c876d5b8dcf441f07d4aebacf0ca3
Alexis West <alexishwest98@gmail.com>
2022-10-20 11:07:39 -0700

    Fixed Review Model

--- backend/db/models/Review.js
+++ backend/db/models/Review.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Review.hasMany(models.ReviewImages, {
+      Review.hasMany(models.ReviewImage, {
         foreignKey: 'reviewId',
         onDelete: 'CASCADE'
       }),
