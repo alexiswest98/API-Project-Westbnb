0bde633da2d5cc1b873690d9498eed98d1802632
Alexis West <alexishwest98@gmail.com>
2022-10-27 13:03:13 -0700

    Possibly found bug, fixed typo for get details spot by id

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -77,7 +77,7 @@ router.get('/:spotId', async (req, res, next) => {
                 attibutes: ["id", "firstName", "lastName"]
             },
         ],
-        group: ["Spot.id", "SpotImages.id", "Owner.id"]
+        group: ["Spot.id", "SpotImage.id", "Owner.id"]
     });
 
     if (!spot) {
