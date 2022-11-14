8924aef6733346d7b86439c1552672d49aa56a6d
Alexis West <alexishwest98@gmail.com>
2022-11-14 08:33:24 -0800

    Added Sign Up Modal

---
+++ frontend/src/store/spots.js
@@ -0,0 +1,17 @@
+import { csrfFetch } from "./csrf";
+
+//TYPE
+
+const GET_ALL_SPOTS = 'spots/getAllSpots';
+const GET_ONE_SPOT = 'spot/getOneSpot';
+const DELETE_A_SPOT = 'spot/deleteSpot';
+const ADD_A_SPOT = "spots/addSpot";
+// const RESET_STATE = 'spot/clearState'
+
+//ACTIONS
+
+
+//THUNKS
+
+
+//REDUCER
